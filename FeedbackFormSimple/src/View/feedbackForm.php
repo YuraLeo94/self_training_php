0325e003cd2a8e0037680624833c8c41765ca55f
YuraLeo94 <jurijsleonovich@inbox.lv>
2024-02-06 16:05:42 +0200

    Implemented feedback form and updated form field component.

---
+++ FeedbackFormSimple/src/View/feedbackForm.php
@@ -0,0 +1,28 @@
+<?php
+
+class FeedbackForm
+{
+
+    public function render($fields, $buttonName = 'submit', $value = "submit", $methodName = 'POST', $serverName = 'PHP_SELF')
+    {
+        echo '<h2>Feedback</h2>';
+        echo '<p class="lead text-center">Leave feedback</p>';
+        echo '<form method="' . $methodName . '" action="' . htmlspecialchars($_SERVER[$serverName]) . '" class="mt-4 w-75">';
+        foreach ($fields as $field) {
+            (
+                new FormField(
+                    $field['wrapperClassName'],
+                    $field['label'],
+                    $field['name'],
+                    $field['type'],
+                    $field['placeholder'],
+                    $field['value']
+                )
+            )->render(!$field['type']);
+        }
+        echo "<div class=\"mb-3\">";
+        echo "<input type=\"submit\" name=\"" . $buttonName . "\" value=\"" . $value . "\" class=\"btn text-capitalize btn-dark w-100\">";
+        echo "</div>";
+        echo '</form>';
+    }
+}
--- FeedbackFormSimple/src/layout.php
+++ FeedbackFormSimple/src/layout.php
@@ -1,6 +1,7 @@
 <?php
 require_once('View/headerView.php');
 require_once('View/formField.php');
+require_once('View/feedbackForm.php');
 
 $headerView = new HeaderView();
 
@@ -9,5 +10,33 @@ $buttons = [
     ['name' => 'Feedback', 'href' => 'feedback.php'],
 ];
 
+$fields = [
+    [
+        'wrapperClassName' => 'mb-3', 
+        'label' => 'Name', 
+        'name' => 'name', 
+        'type' => 'text', 
+        'placeholder' => 'Enter your name', 
+        'value' => ''
+    ],
+    [
+        'wrapperClassName' => 'mb-3', 
+        'label' => 'Email', 
+        'name' => 'email', 
+        'type' => 'email', 
+        'placeholder' => 'Enter your email', 
+        'value' => '',
+    ],
+    [
+        'wrapperClassName' => 'mb-3', 
+        'label' => 'Feedback', 
+        'name' => 'feedback', 
+        'type' => '', 
+        'placeholder' => 'Enter your feedback', 
+        'value' => '',
+    ]
+];
+
 $headerView->renderHeader("Simple Feedback form", $buttons);
+(new FeedbackForm())->render($fields);
 ?>
