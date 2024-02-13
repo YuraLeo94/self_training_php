e6619614d22139b2ca6e063a546b83bf7229298f
YuraLeo94 <jurijsleonovich@inbox.lv>
2024-02-13 15:53:20 +0200

    Added buttons rendering in FeedbackItem component

--- FeedbackFormSimple/src/View/feedbackItem.php
+++ FeedbackFormSimple/src/View/feedbackItem.php
@@ -6,13 +6,30 @@ class FeedbackItem
     public function render($feedback)
     {
         echo '<div class="card my-3 w-75">';
-        echo '<div class="card-body text-center">';
-        echo $feedback['body'];
-        echo '<div class="text-secondary mt-2">';
-        echo 'By' . $feedback['name'];
-        echo $feedback['date'];
-        echo '</div>';
-        echo '</div>';
+            echo '<div class="card-body text-body">';
+                echo '<div class="my-2 d-flex justify-content-between text-dark mt-2">';
+                    echo '<div>' . 'By ' . $feedback['name'] . '</div>';
+                    echo '<div>' . '<span class="fw-bold">Date:</span> ' . $feedback['date'] . '</div>';
+                echo '</div>';
+                echo $feedback['body'];
+                $this->renderBasicButtonsPanel();
+                $this->renderEditModeButtons();
+            echo '</div>';
         echo '</div>';
     }
+
+    public function renderBasicButtonsPanel() {
+        echo '<div class="mt-3 d-flex flex-wrap justify-content-end align-items-center gap-2">';
+            echo '<button class="btn btn-outline-secondary"> Delete </button>';
+            echo '<button class="btn btn-outline-secondary">Edit</button>';
+        echo '</div>';
+    }
+
+    public function renderEditModeButtons() {
+        echo '<div class="mt-3 d-flex flex-wrap justify-content-end align-items-center gap-2">';
+            echo '<button class="btn btn-outline-secondary">Cancel</button>';
+            echo '<button class="btn btn-primary"> Accept </button>';
+        echo '</div>';
+    }
+
 }
