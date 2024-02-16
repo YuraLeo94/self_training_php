5915fbb0956c955c8d600ffbbee96250075f72ab
YuraLeo94 <jurijsleonovich@inbox.lv>
2024-02-16 22:06:51 +0200

    Added on button logic for FeedbackPageController

--- FeedbackFormSimple/src/Control/feedbackPageController.php
+++ FeedbackFormSimple/src/Control/feedbackPageController.php
@@ -14,18 +14,46 @@ class FeedbackPageController
         $this->feedbackModel = $feedbackModel;
     }
 
+    private function refreshView() {
+        header("Location: ".parse_url($_SERVER['REQUEST_URI'])['path']);
+        exit();
+
+    }
+
     public function onEdit(int $value) {
         $this->model->setEditModeFeedbackIndex($value);
-        echo '2' . $this->model->getEditModeFeedbackIndex();
-        // $this->updateView();
+        $_SESSION['editModeFeedbackIndex'] = $value;
+        $this->refreshView();
+    }
+
+    public function onCancel() {
+        $_SESSION['editModeFeedbackIndex'] = null;
+        $this->refreshView();
+    }
+
+    public function onApply() {
+        $_SESSION['editModeFeedbackIndex'] = null;
+        $this->refreshView();
+    }
+
+    public function onDelete(int $value) {
+        // call model method
+        $this->refreshView();
     }
 
     public function updateView($feedbacks = null)
     {
+        $editModeFeedbackIndex = null;
+        if (isset($_SESSION['editModeFeedbackIndex'])) {
+            $editModeFeedbackIndex = $_SESSION['editModeFeedbackIndex'];
+        }
+
         if ($feedbacks === null) {
             $feedbacks = $this->feedbackModel->getFeedbacks();
         }
-        echo '1';
-        $this->view->render($feedbacks, [$this, 'onEdit'], [$this->model, 'getEditModeFeedbackIndex']);
+
+        $this->view->render($feedbacks, $editModeFeedbackIndex);
     }
 }
+
+session_start();
