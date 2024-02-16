ebf9948b16dfd26bfaf0caaa6f23770be63aebe5
YuraLeo94 <jurijsleonovich@inbox.lv>
2024-02-16 21:59:31 +0200

    Updated form field component to display an error

--- FeedbackFormSimple/src/View/formField.php
+++ FeedbackFormSimple/src/View/formField.php
@@ -7,10 +7,11 @@ class FormField
     private $name = '';
     private $type = '';
     private $error = '';
+    private $errorMessage = '';
     private $placeholder = '';
     private $value = '';
 
-    public function __construct($wrapperClassName, $label, $name, $type, $placeholder = '', $value = '')
+    public function __construct($wrapperClassName, $label, $name, $type, $placeholder = '', $value = '', $errorMessage = 'Field is reqired')
     {
         $this->wrapperClassName = $wrapperClassName;
         $this->label = $label;
@@ -18,6 +19,7 @@ class FormField
         $this->type = $type;
         $this->placeholder = $placeholder;
         $this->value = $value;
+        $this->errorMessage = $errorMessage;
     }
 
     function setError($error)
@@ -27,12 +29,25 @@ class FormField
 
     public function render($isTextArea = false)
     {
-        echo "<div class=\"{$this->wrapperClassName}\">";
-        echo "<label for=\"{$this->name}\" class=\"form-label\">{$this->label}</label>";
-        echo !$isTextArea ? "<input type=\"{$this->type}\" class=\"form-control" .
-            (!$this->error ? '' : 'is-invalid') . "\" name=\"{$this->name}\" placeholder=\"{$this->placeholder}\" value=\"{$this->value}\">" :
-            "<textarea  type=\"{$this->type}\" class=\"form-control" .
-            (!$this->error ? '' : 'is-invalid') . "\" name=\"{$this->name}\" placeholder=\"{$this->placeholder}\" value=\"{$this->value}\"></textarea>";
+        $this->errorHandler($this->name);
+        echo "<div class='{$this->wrapperClassName}'>";
+        echo "<label for='{$this->name}' class=\"form-label\">{$this->label}</label>";
+        echo !$isTextArea ? "<input type=\"{$this->type}\" class=\"form-control " .
+        (!$this->error ?: 'is-invalid') . "\" name=\"{$this->name}\" placeholder=\"{$this->placeholder}\" value=\"{$this->value}\">" :
+            "<textarea  type=\"{$this->type}\" class=\"form-control " .
+            (!$this->error ?: 'is-invalid') . "\" name=\"{$this->name}\" placeholder=\"{$this->placeholder}\" value=\"{$this->value}\"></textarea>";
+            
+        echo "<div class='invalid-feedback'>".$this->error."</div>";
         echo '</div>';
+    }
+
+    private function errorHandler($name) {
+        $error = '';
+        if (isset($_POST['submit'])) {
+            if (empty($_POST[$name])) {
+                $error = $this->errorMessage;
+            }
+        }
+        $this->error = $error;
     }
 }
