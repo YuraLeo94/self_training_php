a97278e6c9528101496f458d2e5831be989654c5
YuraLeo94 <jurijsleonovich@inbox.lv>
2024-02-16 22:05:35 +0200

    Implemented action handle to control application behavior

--- FeedbackFormSimple/src/utils/action/index.php
+++ FeedbackFormSimple/src/utils/action/index.php
@@ -1,20 +1,30 @@
 <?php
 
-class HandleActions {
-
-    public function handleActions($feedbackPageController) {
-        if (isset($_GET['action'])) {
+class HandleActions
+{
+    public function handleActions($feedbackPageController)
+    {
+        $action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_SPECIAL_CHARS );
+        if (!$action && isset($_GET['action'])) {
             $action = $_GET['action'];
+        }
+
+        if (!!$action) {
             $index = isset($_GET['index']) ? $_GET['index'] : null;
-        
+
             switch ($action) {
                 case 'edit':
-                    $feedbackPageController->onEdit($index, "Updated Task");
+                    $feedbackPageController->onEdit($index);
+                    break;
+                case 'cancel':
+                    $feedbackPageController->onCancel();
+                    break;
+                case 'apply':
+                    $feedbackPageController->onApply();
                     break;
                 case 'delete':
-                    // $feedbackPageController->deleteTask($index);
+                    $feedbackPageController->onDelete($index);
                     break;
-                // Add other actions as needed
             }
         }
     }
