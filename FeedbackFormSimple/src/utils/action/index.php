28db68f26f0cbc36c81922596c8a2a666c0543e3
YuraLeo94 <jurijsleonovich@inbox.lv>
2024-02-18 21:22:48 +0200

    Added new action cases

--- FeedbackFormSimple/src/utils/action/index.php
+++ FeedbackFormSimple/src/utils/action/index.php
@@ -2,10 +2,9 @@
 
 class HandleActions
 {
-    public function handleActions($feedbackPageController)
+    public function handleActions($feedbackPageController, $userController)
     {
         $action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_SPECIAL_CHARS);
-        $id = filter_input(INPUT_POST, 'apply_id', FILTER_SANITIZE_SPECIAL_CHARS);
 
         if (!$action && isset($_GET['action'])) {
             $action = $_GET['action'];
@@ -25,10 +24,20 @@ class HandleActions
                     $feedbackPageController->onCancel();
                     break;
                 case 'apply':
+                    $id = filter_input(INPUT_POST, 'apply_id', FILTER_SANITIZE_SPECIAL_CHARS);
                     if ($id) {
                         $feedbackPageController->onApply($id);
                     }
                     // display modal error
+                    break;
+                case 'sign_in':
+                    $userController->onLogin();
+                    break;
+                case 'create_acc':
+                    $userController->onCreatAccount();
+                    break;
+                case 'logout':
+                    $userController->onLogout();
                     break;
                 case 'delete':
                     if (isset($_GET['id'])) {
